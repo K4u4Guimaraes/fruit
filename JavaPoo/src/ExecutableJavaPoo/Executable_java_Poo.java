e6dfdf237eeb561b336e6f61f57b5c5876928ce9
K4u4Guimaraes <K4u4Guimaraes>
2023-09-11 04:58:01 -0300

    Java OOP

---
+++ JavaPoo/src/ExecutableJavaPoo/Executable_java_Poo.java
@@ -0,0 +1,10 @@
+package ExecutableJavaPoo;
+
+public class Executable_java_Poo {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ JavaPoo/src/StudentClass/Student.java
@@ -0,0 +1,12 @@
+package StudentClass;
+
+public class Student {
+	String name;
+	int age;
+	String birthday;
+	String rg;
+	String cpf;
+	String mothers_name;
+	String fathers_name;
+	String school;
+}
